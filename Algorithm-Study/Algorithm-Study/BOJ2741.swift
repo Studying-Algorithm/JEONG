8469a20c547f06d753b6dbdb6a728b3cabc6bcde
kwonjeong <kwonjeong9470@gmail.com>
2023-08-13 19:28:48 +0900

    [Setting] gitignore 파일 수정

---
+++ Algorithm-Study/Algorithm-Study/BOJ2741.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm-Study
+//
+//  Created by KJ on 2023/08/13.
+//
+
+import Foundation
+
+let num = Int(readLine()!)!
+
+for i in 1...num {
+    print(i)
+}
+
---
+++ Algorithm-Study/Algorithm-Study/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm-Study
+//
+//  Created by KJ on 2023/08/13.
+//
+
+import Foundation
+
+print("?")
