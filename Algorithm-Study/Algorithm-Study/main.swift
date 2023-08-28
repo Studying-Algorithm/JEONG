8b6a9ba1bdf968332dcb30bc57f887c16ee510ec
kwonjeong <kwonjeong9470@gmail.com>
2023-08-28 19:55:16 +0900

    [Solve] BOJ1934 - 최소공배수

--- Algorithm-Study/Algorithm-Study/main.swift
+++ Algorithm-Study/Algorithm-Study/main.swift
@@ -1,8 +1,26 @@
 //
-//  main.swift
+//  BOJ 1934 - 최소공배수.swift
 //  Algorithm-Study
 //
 //  Created by KJ on 2023/08/28.
 //
 
 import Foundation
+
+for _ in 0..<Int(readLine()!)! {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    var A = input[0]
+    var B = input[1]
+    
+    let result = (A * B) / gcd(A, B)
+    print(result)
+
+}
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    } else {
+        return gcd(b, a % b)
+    }
+}
