c7b3e9ce0407851aa56fd6c1542d908168bcd8ca
kwonjeong <kwonjeong9470@gmail.com>
2023-08-28 17:35:24 +0900

    [Setting] 문제풀이 세팅

--- Algorithm-Study/Algorithm-Study/main.swift
+++ Algorithm-Study/Algorithm-Study/main.swift
@@ -1,27 +1,8 @@
 //
-//  BJ2839 - 설탕 배달
+//  main.swift
 //  Algorithm-Study
 //
-//  Created by KJ on 2023/08/24.
+//  Created by KJ on 2023/08/28.
 //
 
 import Foundation
-
-var N = Int(readLine()!)!
-var result = 0
-
-while N >= 0 {
-    if N % 5 == 0 {
-        result += N / 5
-        N = 0
-        break
-    }
-    N -= 3
-    result += 1
-}
-
-if N == 0 {
-    print(result)
-} else {
-    print(-1)
-}
