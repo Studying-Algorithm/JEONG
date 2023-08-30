40d0d755eaf7d1fab107f9208a5b17a080b41f78
kwonjeong <kwonjeong9470@gmail.com>
2023-08-30 20:31:58 +0900

    [Fail] BOJ1212 - 8진수 2진수

--- Algorithm-Study/Algorithm-Study/main.swift
+++ Algorithm-Study/Algorithm-Study/main.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+let octalNum = readLine()!
+var binaryArr: [Int] = []
+
+for num in octalNum.reversed() {
+    if var n = Int(String(num)) {
+        for _ in 0..<3 {
+            binaryArr.append(Int(n) % 2)
+            n /= 2
+        }
+    }
+}
+
+for _ in 0..<binaryArr.count {
+    if binaryArr.last == 0 {
+        binaryArr.removeLast()
+    } else {
+        break
+    }
+}
+
+print(binaryArr.reversed().map { String($0) }.joined())
