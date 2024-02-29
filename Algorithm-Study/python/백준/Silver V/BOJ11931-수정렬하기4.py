022a2d60e2c1f0450771b911456f522eb55989da
kwonjeong <kwonjeong9470@gmail.com>
2024-02-29 11:19:00 +0000

    [Solve] - BOJ11931 - 수 정렬하기 4

---
+++ Algorithm-Study/python/백준/Silver V/BOJ11931-수정렬하기4.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+array = []
+
+for _ in range(n):
+  array.append(int(input()))
+
+array.sort(reverse = True)
+for i in range(n):
+  print(array[i])
